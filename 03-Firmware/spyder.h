5314f6a74f81b26a8acf2f41506694d373a8fa12
doyungu <d.gu416@gmail.com>
2024-02-15 11:12:24 +0000

    Changed Class File

--- 03-Firmware/spyder.h
+++ 03-Firmware/spyder.h
@@ -1,5 +1,12 @@
+# include "pico/stdlib.h"
+# include "hardware/pwm.h"
+
 # define nosurvo 18;
 # define motordriver 18;
 # define maxduty 8192;
 # define minduty 1638;
 # define pwm 50;
+
+int ServoDriver() {
+
+}
